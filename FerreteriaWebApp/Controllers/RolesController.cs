cc7e43bc94733314e55a7a19323f450f860bed6f
JblSomoza <jhony-bryan@outlook.com>
2025-05-09 00:18:56 -0600

    Se sube roles aun falta validar eliminar y buscar por id

---
+++ FerreteriaWebApp/Controllers/RolesController.cs
@@ -0,0 +1,104 @@
+﻿using FerreteriaWebApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FerreteriaWebApp.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+
+        public async Task<ActionResult> Index()
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:44333/");
+
+            var response = await _httpClient.GetAsync($"rest/api/obtenerRoles");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                var resp = JsonConvert.DeserializeObject<List<RolesModel>>(content);
+
+                return View("Index",resp);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AgregarRol(RolesModel rolesModel)
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:44333/");
+            var json = JsonConvert.SerializeObject(rolesModel);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"rest/api/insertarRol", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contentResponse = await response.Content.ReadAsStringAsync();
+                var resp = JsonConvert.DeserializeObject<ResponseRol>(contentResponse);
+                return new HttpStatusCodeResult(200);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(500);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EditarRol(RolesModel roles)
+        {
+            _httpClient.BaseAddress = new Uri("https://localhost:44333/");
+            
+            var newBody = new
+            {
+                Nombre = roles.Nombre,
+                Sueldo = roles.Sueldo
+            };
+
+            var json = JsonConvert.SerializeObject(newBody);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"rest/api/actualizarRol/{roles.IdRol}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var contentResponse = await response.Content.ReadAsStringAsync();
+                var resp = JsonConvert.DeserializeObject<ResponseRol>(contentResponse);
+                return new HttpStatusCodeResult(200);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(500);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EliminarRol(EliminarRolRequest req)
+        {
+            var id = req.idRol;
+            _httpClient.BaseAddress = new Uri("https://localhost:44333/");
+            var response = await _httpClient.DeleteAsync($"rest/api/eliminarRol/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return new HttpStatusCodeResult(200);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(500);
+            }
+        }
+
+        public class EliminarRolRequest
+        {
+            public int idRol { get; set; }
+        }
+    }    
+}
---
+++ FerreteriaWebApp/Models/RolesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FerreteriaWebApp.Models
+{
+    public class RolesModel
+    {
+        public byte IdRol { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Sueldo { get; set; }
+    }
+
+    public class ResponseRol
+    {
+        public int respuesta { get; set; }
+        public string descripcion_respuesta { get; set; }
+    }
+}
